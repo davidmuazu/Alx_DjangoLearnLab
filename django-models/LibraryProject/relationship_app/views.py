1f7e5b56a3fa438f4f10263c87ad5728443a0dbe
Muazu David Joseph <josephmuazu5@gmail.com>
2025-09-04 01:12:38 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library   
 
 
@@ -13,6 +13,7 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
 
 
 
