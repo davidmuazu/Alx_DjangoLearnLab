f8125c245b1f7515723256672e9493f2347b5e88
Muazu David Joseph <josephmuazu5@gmail.com>
2025-09-10 17:20:16 +0100

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from .models import Book
 from .models import CustomUser
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 # Customize how Book is displayed in the admin
 @admin.register(Book)
@@ -26,3 +28,29 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+# View all books (requires can_view)
+@permission_required("bookshelf.can_view", raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/view_books.html", {"books": books})
+
+# Add a new book (requires can_create)
+@permission_required("bookshelf.can_create", raise_exception=True)
+def add_book(request):
+    # Implementation for creating a book
+    return render(request, "bookshelf/add_book.html")
+
+# Edit book (requires can_edit)
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def edit_book(request, book_id):
+    # Implementation for editing a book
+    return render(request, "bookshelf/edit_book.html")
+
+# Delete book (requires can_delete)
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_book(request, book_id):
+    # Implementation for deleting a book
+    return render(request, "bookshelf/delete_book.html")
+
