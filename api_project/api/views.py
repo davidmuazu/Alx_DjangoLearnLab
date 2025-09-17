d4cc83574e701c23ecdfb74f6a0de5303c6b2cf1
Muazu David Joseph <josephmuazu5@gmail.com>
2025-09-17 14:33:35 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,8 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# New ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
