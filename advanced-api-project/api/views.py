13779977b57245e8acc66e2af8a04641a5f7caf5
Muazu David Joseph <josephmuazu5@gmail.com>
2025-09-26 00:13:02 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, filters
+from rest_framework import generics
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
@@ -54,6 +55,7 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
 
 
 
