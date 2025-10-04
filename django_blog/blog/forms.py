4bdaa2e4e3bb2208005a7ae9a5cd1327efd51c1b
Muazu David Joseph <josephmuazu5@gmail.com>
2025-10-04 02:09:14 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -23,3 +23,14 @@ class PostForm(forms.ModelForm):
             "title": forms.TextInput(attrs={"class": "form-control"}),
             "content": forms.Textarea(attrs={"class": "form-control", "rows": 8}),
         }
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 3}),
+        label="",
+        help_text="Write your comment here."
+    )
+
+    class Meta:
+        model = Comment
+        fields = ["content"]
