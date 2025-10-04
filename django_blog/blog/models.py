cd9169dd132ba8f498f7818512705902439ea3ca
Muazu David Joseph <josephmuazu5@gmail.com>
2025-10-04 02:08:28 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
 
 
@@ -13,6 +15,26 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk})
+    
+class Comment(models.Model):
+    post = models.ForeignKey(
+        "Post",
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
