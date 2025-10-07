2a2ba1f1b3c10e60973e50393d12da3d3355b80d
Muazu David Joseph <josephmuazu5@gmail.com>
2025-10-07 01:15:56 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    tags = models.ManyToManyField("Tag", blank=True, related_name="posts")
 
     def __str__(self):
         return self.title
@@ -38,3 +39,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("posts-by-tag", kwargs={"tag_name": self.name})
