a396c416a39a35205314cc2b6622cd9c997e18f8
Muazu David Joseph <josephmuazu5@gmail.com>
2025-10-07 01:22:58 +0100

    Update admin.py

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 # Register your models here.
 @admin.register(Post)
@@ -13,3 +13,9 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ("post", "author", "created_at")
     list_filter = ("created_at", "author")
     search_fields = ("content",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
