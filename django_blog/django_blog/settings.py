88c4739d6ab59c9420d38ccc5e9978039419a5e4
Muazu David Joseph <josephmuazu5@gmail.com>
2025-10-07 01:26:09 +0100

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -138,5 +139,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
